b68a0f032d8a8320751e65d80e2695e96476ca40
AGNESGITAU <agnesgitau40@gmail.com>
2024-04-07 10:48:20 +0300

    functions-continued

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+// Define saturdayFun function
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+//Define mondayWork function as a function expression
+const mondayWork = function(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`;
+}
+//Define wrapAdjective function
+function wrapAdjective(ines = "*"){
+    return function(adjective){
+        return `You are ${ines}${adjective}${ines}!`;
+    }
+}
